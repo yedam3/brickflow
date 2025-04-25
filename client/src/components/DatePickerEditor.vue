2057c6c5ad2407e9635882552a4d8aaecb93e3d6
hyunooki <aosdnr54321@gmail.com>
2025-04-26 03:53:04 +0900

    1231241

---
+++ client/src/components/DatePickerEditor.vue
@@ -0,0 +1,35 @@
+<template>
+  <input
+    type="date"
+    :value="value"
+    @input="onInput"
+    @blur="onBlur"
+    class="ag-input"
+  />
+</template>
+
+<script>
+export default {
+  props: ['value'], 
+  data() {
+    return {
+      internalValue: this.value
+    };
+  },
+  methods: {
+    onInput(event) {
+      this.internalValue = event.target.value;
+    },
+    onBlur() {
+      this.params.stopEditing();
+    },
+    getValue() {
+      return this.internalValue;
+    }
+  },
+  mounted() {
+    // ag-Grid는 props 말고 this.params로 다양한 정보를 전달함
+    this.internalValue = this.params.value;
+  }
+};
+</script>
