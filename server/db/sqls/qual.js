24e75e2729aad8cb4c34c36362798f94080591cd
ddul22 <kjj0757@naver.com>
2025-04-28 02:39:43 +0900

    04.27

--- server/db/sqls/qual.js
+++ server/db/sqls/qual.js
@@ -1,3 +1,18 @@
+// db/sqls/qual.js
+// Table : b_admin_bom
+
+// bom관리
+const bomlist =
+`SELECT BOM_code, 
+        mat_code, 
+        prod_code, 
+        quantity, 
+        bom_add_date, 
+        pass_or_not, 
+        emp_code
+FROM BOM;`
+
 module.exports = {
+    bomlist,
 
 };
--- server/services/qualService/qual_service.js
+++ server/services/qualService/qual_service.js
@@ -1,6 +1,11 @@
 const mariaDB = require("../../db/mapper.js");
 const { convertObjToAry } = require("../../utils/converts.js");
 
+const findAll = async ()=>{
+    let list = await mariadb.query('boardList'); 
+    return list;
+  }
+
 module.exports = {
     
 }
