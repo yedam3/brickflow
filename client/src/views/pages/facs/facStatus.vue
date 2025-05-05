c2a201d6d84709c3f6d367a54fc208d46e8bd656
eunchong00 <jeongeunchong83@gmail.com>
2025-05-06 01:54:08 +0900

    250506

--- client/src/views/pages/facs/facStatus.vue
+++ client/src/views/pages/facs/facStatus.vue
@@ -14,9 +14,10 @@
       <label class="form-check-label" for="inlineRadio2">비가동</label>
     </div>
   </div>
+  <br>
   <div class="col" style="margin-right: 50px;">
             <div class="ag-wrapper d-flex justify-content-center">
-                <ag-grid-vue class="ag-theme-alpine custom-grid-theme" style="width: 1000px; height: 400px;"
+                <ag-grid-vue class="ag-theme-alpine custom-grid-theme" style="width: 100%; height:  500px;"
                     :columnDefs="columnDefs" :rowData="rowData" :gridOptions="gridOptions"
                     @cellClicked="comCellClicked">
                 </ag-grid-vue>
@@ -37,6 +38,14 @@
   },
   data() {
     return{
+      rowData:[{
+        fac_code: "",
+        model_name: "",
+        fac_location:"",
+        fac_pattern:"",
+        employee_code:"",
+        fac_status:""
+      }],
       columnDefs:[
         { field:"fac_code", headerName: "설비코드", flex: 2 },
         { field:"model_name", headerName: "설비이름", flex: 2 },
@@ -44,8 +53,21 @@
         { field:"fac_pattern", headerName: "설비유형", flex: 2 },
         { field:"employee_code", headerName: "담당자", flex: 2 },
         { field:"fac_status", headerName: "설비상태", flex: 2 },
-      ]
+      ],
+      gridOptions:{
+        pagination: true,
+          paginationPageSize: 5,
+          paginationPageSizeSelector: [5, 10, 20, 50],
+          overlayNoRowsTemplate: '표시할 값이 없습니다.',
+        defaultColDef: {
+         sortable: true, //정렬가능
+         resizable: true //마우스드래그
+      }
+     }
     }
+  },
+  methods:{
+
   },
 }
 </script>
--- server/routers/facRouter/fac_router.js
+++ server/routers/facRouter/fac_router.js
@@ -66,7 +66,13 @@ router.get ('/facList', async (req, res)=>{
     res.send(list);
 })
 
-//테스ㅡㅌㅊ
+router.get('/facStatus', async (req, res)=>{
+    const { facCode} = req.query;
+    let list = await facService.statuFac(facCode).catch((err) => console.log(err));
+    res.send(list);
+})
+
+
 
 
 module.exports = router;
